ac53b26fcf72f3e7f5f8f6978ac1d825f469f5f8
TravisHeffelfinger <36177937+TravisHeffelfinger@users.noreply.github.com>
2020-03-12 11:22:11 -0600

    added Material Design
    added initial styles to Material design
    added functionality to detect enter key input and check to see if input field is empty

--- main.js
+++ main.js
@@ -4,14 +4,59 @@ let listObject = {
         listText: [],
         completed: false
     }
+/* if(document.getElementById('sample3').value == ''){
+        document.getElementById('createButton');
+}else if (document.getElementById('sample3' != undefined)) {
+    document.getElementById('createButton');
+}; */
 
-    const listTemplate = `<div class="list-item" style="border:1px solid red;">
-                    <span>Name: New Thing<span>
-                    <ul>
-                        <li>test</li>
-                    </ul>
-                  </div>`;
-     
+//function submitText() {
+/*     document.getElementById("sample3")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("createButton").click();
+        document.getElementById('sample3').value == '';
+    }
+}); */
+
+function searchKeyPress(e)
+{
+    // look for window.event in case event isn't passed in
+    e = e || window.event;
+    if (e.keyCode == 13) {
+        document.getElementById('createButton').click();
+        document.getElementById('sample3').value="";
+        return false;
+    }
+    return true;
+}
+    /* if (event.keyCode == 13 && document.getElementById('sample3').onfocus == true){
+        document.getElementById('createButton').click(); 
+        document.getElementById('sample3').value = '';} */
+//}
+
+function checkToEnableButton() {
+    if(document.getElementById("sample3").value == "") { 
+           document.getElementById('createButton').disabled = true; 
+       } else { 
+           document.getElementById('createButton').disabled=false;
+       }
+   }
+
+    function createListObjTest() {
+        //get input from the text fields
+       let listName =  document.getElementById('sample3').value.trim();
+       let listContent = document.getElementById('sample5').value.trim();
+        //store input into listObject and create a key for list Master
+        listObject.name.push(listName);
+        listObject.listText.push(listContent);
+        //set key for new obj to master list 
+        listObject.listKey.push(listObject.name.length - 1);
+        //console.log(listObject);
+        localStorage.setItem('ListObject', JSON.stringify(listObject));
+        generateList(listObject.listKey.length - 1);
+    }
 
 //creates a new list item and stores it in local storage as an object
 function createListObj() {
@@ -37,16 +82,23 @@ function retrieveListObject() {
 //takes the list item at 'key' position and displays it in html
 function generateList(key) {
     const newList = retrieveListObject();
-    const container = document.getElementById('test-id');
-    const currentKey = key;
-    const list = `<div class="list-item" style="border:1px solid red;">
-                    <span>Name: ${newList.name[currentKey]}<span>
-                    <ul>
-                        <li>${newList.listText[currentKey]}</li>
-                    </ul>
-                  </div>`;
-    
-    container.innerHTML += list;
+    //const container = document.getElementById('page-content');
+    const list = `
+    <div class="test-card mdl-card mdl-shadow--2dp">
+        <div class="mdl-card__title">
+            <h2 class="mdl-card__title-text">${newList.name[key]}</h2>
+        </div>
+        <div class="mdl-card__supporting-text">
+            <ul class="demo-list-item mdl-list">
+            </ul>
+        </div>
+        <div class="mdl-card__actions mdl-card--border">
+                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" onclick="testLiGenerate()">
+                New List Item
+                </a>
+          </div>      
+    </div>`; 
+    document.getElementsByClassName('page-content')[0].innerHTML += list;
     //test Template string
 }
 //display lists from local storage when page is loaded
@@ -62,10 +114,8 @@ function reloadLists() {
 
 //generates the html for the list item
 function generateListHTML(key) {
-    
+
 }
-
-
 
 //for testing. clears the object
 function clearListObject() {
@@ -80,3 +130,10 @@ function clearListObject() {
 function clearLocalStorage() {
     localStorage.clear();
 }
+
+function testLiGenerate() {
+    let listTemplate = `<li class="mdl-list__item"><span class="mdl-list__item-primary-content">sample Text</span></li>`;
+    document.getElementsByClassName('demo-list-item')[0].innerHTML += listTemplate;
+}
+
+
